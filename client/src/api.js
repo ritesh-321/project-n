6dc13b81a59b8039698b3de389ba8567711be3cd
ritesh-321 <nepalhotspot@gmail.com>
2025-09-05 23:56:37 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // This is good
+  baseURL: "https://project-n-se6h.onrender.com/api", // This is good
   // ❌ Don't manually set Content-Type here — let axios handle it for file uploads
 });
 
